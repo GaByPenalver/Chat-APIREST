69140daf69ee7c774adc1b1f045864d46437cbd4
GaBy Peñalver <Gabypealver14@gmail.com>
2024-11-15 21:54:14 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import User from "./components/User.vue";
+import Message from "./components/Message.vue";
+
+export default {
+  name: "App",
+  components: {
+    User,
+    Message,
+  },
+  data() {
+    return {
+      messages: [], 
+    };
+  },
+  methods: {
+    handleSend(message) {
+      this.messages.push(message); 
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <User position="left" @send="handleSend" />
+    <Message :messages="messages" />
+    <User position="right" @send="handleSend" />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start; 
+  width: 100%; 
+  max-width: 1280px;
+  margin: auto;
+  padding: 10px;
+  gap: 15px;
+}
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <div
+      v-for="(message, index) in messages"
+      :key="index"
+      class="messageUser"
+      :class="{'align-left': message.aligne === 'left', 'align-right': message.aligne === 'right'}"
+    >
+      <p>{{ message.name }}</p>
+      <p class="pmessage" :style="{ backgroundColor: message.color }">{{ message.text }}</p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Message',
+    props:{
+        messages: Array,
+
+    },
+};
+</script>
+
+<style scoped>
+.container {
+  height: 550px;
+  width: 25%;
+  margin: 5px;
+  border: 2px solid #b8bab9;
+  background-color: #d7dbd8;
+  overflow-y: auto;
+  padding-top: 15px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+
+.messageUser {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  max-width: 70%;
+}
+
+.align-left {
+  align-self: flex-start; 
+}
+
+.align-right {
+  align-self: flex-end; 
+}
+
+.pmessage {
+  width: max-content;
+  padding: 10px;
+  border-radius: 25px;
+}
+
+p {
+  margin: 2px 0;
+  padding-left: 2px;
+}
+</style>
